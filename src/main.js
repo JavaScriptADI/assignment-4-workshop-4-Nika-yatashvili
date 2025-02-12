7fcffb04726ad426143463d8acf88e1d62fd033c
Nika-yatashvili <nika.katashvili2@gmail.com>
2025-02-12 02:50:34 -0800

    assignment4

--- src/main.js
+++ src/main.js
@@ -1,25 +1,28 @@
 // Write your code here
-
-// let array = [1,2,7]
-
-// array.splice(2,0,3,4,5,6)
-// console.log(array)
-
-
-// let colours = ['Red' , 'green' , 'blue' ,'yellow']
-
-// colours.splice(1,0, 'blue')
-// console.log(colours)
-
+// QUIZZ
+/*
+1. array.push()
+2.array.shift()
+3. splice changes the original array, slice returns new.
+4.array.length
+5.array.includes()
+6. messy
+7.let array = [1,3,4] ,
+  let array1 = [2,5,7]
+  let array2 = array.concat(array2)
+8.array.reverse()
+9.?
+10.?
+*/
 
 
 // assigments 
 // task1 
-let array = [1,2,3,4,5,6,7,8,9,10];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(array[2]);
 
-array.splice(4,1,15);
+array.splice(4, 1, 15);
 console.log(array);
 
 array.push(14);
@@ -29,9 +32,9 @@ console.log(array);
 
 // task2 
 
-let array2 = ['strawberry', 'orange', 'blueberry', 'watermelon' ];
+let array2 = ['strawberry', 'orange', 'blueberry', 'watermelon'];
 
-array2.splice(1,1, "pineapple");
+array2.splice(1, 1, "pineapple");
 array2.push("orange");
 console.log(array2);
 
@@ -48,27 +51,27 @@ console.log(firstRemovedItem);
 
 // task3 
 console.log(`task 3 colours`);
-let colours = ['Red' , 'green' , 'blue' ,'yellow', 'cyan'];
+let colours = ['Red', 'green', 'blue', 'yellow', 'cyan'];
 
-for(let i =0; i<5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(colours[i]);
 }
 
 // task4 
 console.log(`task 4`);
 
-let array3 = [1,2,3,4,5,6,7] ;
+let array3 = [1, 2, 3, 4, 5, 6, 7];
 
-array3.splice(1,1);
+array3.splice(1, 1);
 
 console.log(array3);
-array3.splice(1,1) ;
+array3.splice(1, 1);
 console.log(array3);
 
-array3.splice(1,0, 14,15);
+array3.splice(1, 0, 14, 15);
 console.log(array3);
 
-let newArray = array3.slice(4,7);
+let newArray = array3.slice(4, 7);
 
 console.log(newArray);
 
@@ -77,17 +80,32 @@ let names = ['lukas', 'alex', 'ben', 'nick'];
 
 names.sort();
 console.log(names);
-names.reverse() 
-console.log(names)
+names.reverse();
+console.log(names);
 
-console.log('task6')
+console.log('task6');
 // task6 
-let numbers = [1,2,3,4,5,6,7]
+let numbers = [1, 2, 3, 4, 5, 6, 7];
 
 
-console.log(numbers.indexOf(4))
-console.log(numbers.includes(8))
+console.log(numbers.indexOf(4));
+console.log(numbers.includes(8));
 
+console.log("task9");
+// task 9 
+let nums = [1, 2, 3, 4];
+let nums2 = [5, 6, 7, 8];
+let nums1nums2 = nums.concat(nums2);
+console.log(nums1nums2);
 
+let numsPlusNums2 = [...nums, ...nums2];
+
+console.log(numsPlusNums2);
+
+// task10 
+
+let theNumbers = [2, 4, 6, 8];
+theNumbers.reverse();
+console.log(theNumbers);
 
 
